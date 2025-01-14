531a801633dfb545429cc11e9c6560b405e721af
diwasbk <diwasbk903@gmail.com>
2025-01-15 01:53:59 +0545

    Bottom Navigation Bar

---
+++ app/src/main/java/com/example/kotlinapplication/ui/activity/NavigationActivity.kt
@@ -0,0 +1,50 @@
+package com.example.kotlinapplication.ui.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.kotlinapplication.R
+import com.example.kotlinapplication.databinding.ActivityNavigationBinding
+import com.example.kotlinapplication.ui.fragment.HomeFragment
+import com.example.kotlinapplication.ui.fragment.NotificationFragment
+import com.example.kotlinapplication.ui.fragment.ProfileFragment
+import com.example.kotlinapplication.ui.fragment.SearchFragment
+
+class NavigationActivity : AppCompatActivity() {
+    lateinit var binding: ActivityNavigationBinding
+    // Function to implement Fragments
+    fun replaceFragment(fragment: Fragment) {
+        val fragmentManager : FragmentManager = supportFragmentManager
+        val fragmentTransaction : FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameNavigation,fragment)
+        fragmentTransaction.commit()
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityNavigationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // to show home fragment by default
+        replaceFragment(HomeFragment())
+        binding.bottomNavigationView.setOnItemSelectedListener {menu->
+            when(menu.itemId){
+                R.id.navHome -> replaceFragment(HomeFragment())
+                R.id.navSearch -> replaceFragment(SearchFragment())
+                R.id.navNotification -> replaceFragment(NotificationFragment())
+                R.id.navProfile -> replaceFragment(ProfileFragment())
+                else -> {}
+            }
+            true
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
